3b9dd26dce6b7f25cdbe03857c32ddecd044f551
adamhahnsolo <ahahn@live.com>
2017-01-10 13:26:03 -0600

    commeted n stuff

--- MiddleMan/Controllers/DisplayController.cs
+++ MiddleMan/Controllers/DisplayController.cs
@@ -12,6 +12,11 @@ namespace MiddleMan.Controllers
         // GET: Display
         public ActionResult WhatNext(int Id)
         {
+            //open Json text file
+            //iterate through the items in Json
+            //and find the item where ID == input ID
+            //once found return that item
+            //else send new device
             var r = new Devices(); 
             r.Id = 1;
             r.Color = "blue";
--- MiddleMan/Controllers/UIController.cs
+++ MiddleMan/Controllers/UIController.cs
@@ -12,6 +12,14 @@ namespace MiddleMan.Controllers
         // GET: Display
         public ActionResult ChangeColor(int Id, string color)
         {
+            //take the ajax hex code and ID and validate it
+            //if it is not valid return status failure
+            //open json test file
+            //find the ID where the ID == the ajax ID sent
+            //change the hex code sent with the ajax next to that ID
+            //save the json file
+            //return staus success
+            //put the hex code in the json with the corresponding ID
             var retVal = new {
                 status = "success",
                 message = ""
