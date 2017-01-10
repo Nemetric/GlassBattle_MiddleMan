afe9a8985545b75d5e66cf68a04401c276ef0390
adamhahnsolo <ahahn@live.com>
2017-01-10 13:16:08 -0600

    Added retVal

---
+++ MiddleMan/Controllers/UIController.cs
@@ -0,0 +1,23 @@
+﻿using MiddleMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiddleMan.Controllers
+{
+    public class UIController : Controller
+    {
+        // GET: Display
+        public ActionResult ChangeColor(int Id, string color)
+        {
+            var retVal = new {
+                status = "success",
+                message = ""
+            };
+
+            return Json(retVal,JsonRequestBehavior.AllowGet );
+        }
+    }
+}
