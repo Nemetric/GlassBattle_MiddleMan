b64588b3aca73606f7674726ac3818568e2c5e98
Rcdude27 <bontzbryce@gmail.com>
2017-01-10 12:56:18 -0600

    Initial Checkin

---
+++ MiddleMan/Controllers/DisplayController.cs
@@ -0,0 +1,22 @@
+﻿using MiddleMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiddleMan.Controllers
+{
+    public class DisplayController : Controller
+    {
+        // GET: Display
+        public ActionResult WhatNext(int Id)
+        {
+            var r = new Devices(); 
+            r.Id = 1;
+            r.Color = "blue";
+            r.FriendlyName = "asdf";
+            return Json(r,JsonRequestBehavior.AllowGet );
+        }
+    }
+}
---
+++ MiddleMan/Models/Devices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace MiddleMan.Models
+{
+    [DataContract]
+    public class Devices
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Color { get; set; }
+        [DataMember]
+        public string FriendlyName { get; set; }
+
+
+       
+    }
+}
